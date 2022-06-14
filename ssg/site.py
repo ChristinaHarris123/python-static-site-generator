61de66ac015e8cf72c89e845f49cae3d49995e58
Christina Harris <harrischristina4688@gmail.com>
2022-06-14 18:52:37 -0400

    changed doc format

--- ssg/site.py
+++ ssg/site.py
@@ -9,15 +9,12 @@ class Site:
 
 def create_dir(self, path):
    directory = self.dest / path.relative_to(self.source)
-
    directory.mkdir(parents=True, exist_ok=True)
      
 
 def build(self):
     self.dest.mkdir(parents=True, exists_ok=True)
-
     for path in self.source.rglob("*"):
         if path.is_dir():
             self.create.dir(path)
 
-            
