b2417c877c0a3b2c4fbe774dceb66b498ba32811
Christina Harris <harrischristina4688@gmail.com>
2022-07-05 16:51:50 -0400

    added the missing s on parents

--- ssg/site.py
+++ ssg/site.py
@@ -8,10 +8,10 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-        directory.mkdir(parent=True, exist_ok=True)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
-        self.dest.mkdir(parent=True, exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
